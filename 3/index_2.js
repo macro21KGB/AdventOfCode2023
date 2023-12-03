0e6defba89c5972631f4912bf975ce8868a13760
Mario De Luca <bloodcreed21@gmail.com>
2023-12-03 23:37:08 +0100

    solved both part of day 3

--- 3/index_2.js
+++ 3/index_2.js
@@ -16,120 +16,151 @@ What is the sum of all of the gear ratios in your engine schematic?
  * @param {number} startIndex 
  * @param {number} endIndex 
  * @param {Map<number, Part[]} mapOfParts 
- * @returns {Part | null}
+ * @returns {Part[] | null}
  */
 const checkPartsLine = (lineIndex, startIndex, endIndex, mapOfParts) => {
-    const leftStart = startIndex - 1 < 0 ? 0 : startIndex - 1;
-    const rightEnd = endIndex + 1 > splittedInputData.length - 1 ? endIndex : endIndex + 1;
-    const stringToSearchSplitted = splittedInputData[lineIndex].slice(leftStart, rightEnd).split("");
-
     const parts = mapOfParts.get(lineIndex);
-    let foundPart = null;
+    let foundParts = [];
 
     if (!parts)
         return null;
 
-    stringToSearchSplitted.forEach((char, index) => {
-        if (char !== "*" && char !== ".") {
-            if (checkIfPositionInteresectPart(lineIndex, leftStart + index, mapOfParts)) {
-                const part = parts.find((part) => {
-                    return index >= part.start && index <= part.end;
-                });
 
-                if (part) {
-                    foundPart = part;
-                    return;
-                }
-            }
+    parts.forEach((part) => {
+        if (part.end >= startIndex - 1 && part.start <= endIndex) {
+            foundParts.push(part);
         }
-
     });
 
-    return foundPart;
+    return foundParts ? foundParts : null;
 };
 
-const inputData = await fs.readFile("./test_input1.txt", "utf-8");
-const splittedInputData = inputData.split("\n");
 
-
-const allDigitsMatchList = inputData.matchAll(/\d+/g);
-
-const allDigits = [...allDigitsMatchList];
-
-
-const partsMap = new Map();
-allDigits.forEach((digit) => {
-    const currentLineIndex = inputData.slice(0, digit.index).split("\n").length - 1;
-    if (partsMap.has(currentLineIndex)) {
-        partsMap.get(currentLineIndex).push({
-            start: digit.index,
-            end: digit.index + digit[0].length,
-            number: parseInt(digit[0]),
-            lineIndex: currentLineIndex
-        });
-    } else {
-        partsMap.set(currentLineIndex, [{
-            start: digit.index,
-            end: digit.index + digit[0].length,
-            number: parseInt(digit[0]),
-            lineIndex: currentLineIndex
-        }]);
-    }
-});
 
 
 /**
  * 
- * @param {number} lineIndex 
- * @param {number} selectedIndex 
- * @param {Map<number, Part[]>} mapOfParts 
+ * @param {string} inputData 
+ * @returns {Map<number, Part[]>}
  */
-const checkIfPositionInteresectPart = (lineIndex, selectedIndex, mapOfParts) => {
+const createPartsMap = (inputData) => {
+    const splittedInputData = inputData.split("\n");
+    const partsMap = new Map();
 
-    const parts = mapOfParts.get(lineIndex);
-    const lengthOfLine = splittedInputData[lineIndex].length;
+    splittedInputData.forEach((line, indexLine) => {
+        const matches = line.matchAll(/\d+/g);
 
-    if (!parts)
-        return false;
+        const digits = [...matches];
 
-    const part = parts.find((part) => {
-        return selectedIndex >= part.start % lengthOfLine && selectedIndex <= part.end % lengthOfLine;
+        digits.forEach((digit) => {
+            const startIndex = digit.index;
+            const endIndex = digit.index + digit[0].length - 1;
+            const part = {
+                start: startIndex,
+                end: endIndex,
+                number: parseInt(digit[0]),
+                lineIndex: indexLine
+            };
+
+            if (partsMap.has(indexLine)) {
+                const parts = partsMap.get(indexLine);
+                parts.push(part);
+                partsMap.set(indexLine, parts);
+            } else {
+                partsMap.set(indexLine, [part]);
+            }
+        });
     });
 
-    return !!part;
-};
+    return partsMap;
+}
 
-inputData.split("\n").forEach((line, indexLine) => {
-    const matches = line.matchAll(/\*/g);
+// iterate over every line
 
-    const gearSymbols = [...matches];
+/**
+ * 
+ * @param {string} inputData 
+ */
+const checkGear = (inputData) => {
+    const allGearsValid = [];
+    const splittedInputData = inputData.split("\n");
+    const partsMap = createPartsMap(inputData);
+    let totalSumOfGearRatio = 0;
 
-    gearSymbols.forEach((gearSymbol) => {
-        const startIndex = gearSymbol.index;
-        const endIndex = gearSymbol.index + 1;
+    inputData.split("\n").forEach((line, indexLine) => {
+        const matches = line.matchAll(/\*/g);
 
-        // check every parts around the gear, in total of 26 spaces
-        let currentLineIndex = indexLine;
+        const gearSymbols = [...matches];
+
+        gearSymbols.forEach((gearSymbol) => {
+            const startIndex = gearSymbol.index;
+            const endIndex = gearSymbol.index + 1;
+            const partsAroundGear = []
+
+            // check every parts around the gear, in total of 26 spaces
+            let currentLineIndex = indexLine;
 
 
-        const upLine = currentLineIndex - 1 < 0 ? 0 : currentLineIndex - 1;
-        const downLine = currentLineIndex + 1 > splittedInputData.length - 1 ? currentLineIndex : currentLineIndex + 1;
+            const upLine = currentLineIndex - 1 < 0 ? null : currentLineIndex - 1;
+            const downLine = currentLineIndex + 1 > splittedInputData.length - 1 ? null : currentLineIndex + 1;
 
-        const partUp = checkPartsLine(upLine, startIndex, endIndex, partsMap);
-        const partDown = checkPartsLine(downLine, startIndex, endIndex, partsMap);
-        const partCurrent = checkPartsLine(currentLineIndex, startIndex, endIndex, partsMap);
+            if (upLine !== null) {
+                const partUp = checkPartsLine(upLine, startIndex, endIndex, partsMap);
+                if (partUp) {
+                    partUp.forEach((part) => {
+                        partsAroundGear.push(part.number);
+                    });
 
-        // if excatly 2 parts around the gear, then it's a gear and we can calculate the ratio
-        const partsAroundGear = [partUp, partDown, partCurrent].filter((part) => part !== null);
+                }
+            }
 
-        if (partsAroundGear.length === 2) {
-            const gearRatio = partsAroundGear[0].number * partsAroundGear[1].number;
-        }
+            if (downLine !== null) {
+                const partDown = checkPartsLine(downLine, startIndex, endIndex, partsMap);
+                if (partDown) {
+                    partDown.forEach((part) => {
+                        partsAroundGear.push(part.number);
+                    });
+                }
+            }
+
+            const partCurrent = checkPartsLine(currentLineIndex, startIndex, endIndex, partsMap);
+            if (partCurrent) {
+                partCurrent.forEach((part) => {
+                    partsAroundGear.push(part.number);
+                });
+            }
+            // if excatly 2 parts around the gear, then it's a gear and we can calculate the ratio
+            if (partsAroundGear.length === 2) {
+                const gearRatio = partsAroundGear[0] * partsAroundGear[1];
+                allGearsValid.push(partsAroundGear)
+                totalSumOfGearRatio += gearRatio;
+            }
+
+
+        });
 
 
     });
 
 
-});
+    return {
+        allGearsValid,
+        totalSumOfGearRatio
+    };
+}
 
 
+const inputDataRaw = await fs.readFile("./input.txt", "utf-8");
+
+
+const {
+    allGearsValid,
+    totalSumOfGearRatio
+} = checkGear(inputDataRaw);
+
+console.log(totalSumOfGearRatio);
+
+export {
+    checkPartsLine,
+    checkGear
+}
