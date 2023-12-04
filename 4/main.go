f367769d91a89a79dccc3cde510553c65a28157d
Mario De Luca <bloodcreed21@gmail.com>
2023-12-04 13:35:51 +0100

    first star done, working on the second one

--- 4/main.go
+++ 4/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -13,31 +12,65 @@ type Card struct {
 	input          string
 	winningNumbers []int
 	guessedNumbers []int
+	score          int
+	cardNumber     int
 }
 
-func ConvertStringArrayToInt(arr []string) ([]int, error) {
-	result := make([]int, len(arr))
-	for i, s := range arr {
-		num, err := strconv.Atoi(s)
-		if err != nil {
-			return nil, err
+func ConvertStringArrayToInt(arr []string) []int {
+
+	results := make([]int, 0)
+	for _, v := range arr {
+		convertedValue, error := strconv.Atoi(v)
+		if error == nil {
+			results = append(results, convertedValue)
 		}
-		result[i] = num
+
 	}
-	return result, nil
+	return results
 }
 
 func (c *Card) ParseInput() {
 	splitted := strings.Split(c.input, ":")
+	c.cardNumber, _ = strconv.Atoi(strings.Split(splitted[0], " ")[1])
 	guessedAndWinningNumbers := strings.Split(splitted[1], "|")
 
-	parsedWin, _ := ConvertStringArrayToInt(strings.Split(guessedAndWinningNumbers[0], " "))
-	parsedGuess, _ := ConvertStringArrayToInt(strings.Split(guessedAndWinningNumbers[1], " "))
+	parsedWin := ConvertStringArrayToInt(strings.Split(guessedAndWinningNumbers[0], " "))
+	parsedGuess := ConvertStringArrayToInt(strings.Split(guessedAndWinningNumbers[1], " "))
 
-	fmt.Println("Converting")
 	c.winningNumbers = parsedWin
 	c.guessedNumbers = parsedGuess
 
+}
+
+func (c *Card) CalculateScore() int {
+
+	c.score = 0
+	for _, guessedNumber := range c.guessedNumbers {
+		for _, winningNumber := range c.winningNumbers {
+			if guessedNumber == winningNumber {
+				if c.score == 0 {
+					c.score = 1
+				} else {
+					c.score *= 2
+				}
+			}
+		}
+	}
+
+	return c.score
+}
+
+func (c *Card) CalcualteDuplicates(cards []Card) []Card {
+	duplicates := make([]Card, 0)
+	for _, guessedNumber := range c.guessedNumbers {
+		for _, winningNumber := range c.winningNumbers {
+			if guessedNumber == winningNumber {
+				clonedCard := *c
+				duplicates = append(duplicates, clonedCard)
+			}
+		}
+	}
+	return duplicates
 }
 
 func CreateNewCard(input string) Card {
@@ -46,16 +79,19 @@ func CreateNewCard(input string) Card {
 		input:          input,
 		winningNumbers: make([]int, 0),
 		guessedNumbers: make([]int, 0),
+		score:          0,
 	}
 
 	newCard.ParseInput()
+	newCard.CalculateScore()
 
 	return newCard
 }
 
 func main() {
 
-	file, err := os.Open("./input.txt")
+	// First Star
+	file, err := os.Open("./test_input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -65,14 +101,39 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	// Read the file line by line
-	scanner.Scan()
-	line := scanner.Text()
-	card := CreateNewCard(line)
+	cards := make([]Card, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		card := CreateNewCard(line)
+		cards = append(cards, card)
+	}
 
-	fmt.Println(card.guessedNumbers)
+	sum := 0
+	for _, card := range cards {
+		sum += card.score
+	}
+
+	log.Println("Total score is: ", sum)
 
 	// Check for any errors during scanning
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	// Second Star
+	// Create a scanner to read the file line by line
+	scanner = bufio.NewScanner(file)
+	cards = make([]Card, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		card := CreateNewCard(line)
+		cards = append(cards, card)
+	}
+
+	
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 }
